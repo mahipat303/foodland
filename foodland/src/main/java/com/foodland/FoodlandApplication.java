0abe8edcefdf61a323adc65f7c27a1c1596bb44a
Mahipat Parmar <memahipat303@gmail.com>
2022-12-13 20:17:54 +0530

    springboot project created

---
+++ foodland/src/main/java/com/foodland/FoodlandApplication.java
@@ -0,0 +1,14 @@
+package com.foodland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodlandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodlandApplication.class, args);
+		
+	}
+
+}
