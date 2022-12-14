ae2d6faf951a7f203eb6a82c0bcc043965f64bd3
Arup Mahato <arup71062@gmail.com>
2022-12-14 06:39:06 +0530

    Restaurent added

---
+++ foodland/src/main/java/com/foodland/serviceImpl/RestaurantServiceImpl.java
@@ -0,0 +1,10 @@
+package com.foodland.serviceImpl;
+
+import org.springframework.stereotype.Service;
+
+import com.foodland.service.RestaurentService;
+
+@Service
+public class RestaurantServiceImpl implements RestaurentService {
+
+}
