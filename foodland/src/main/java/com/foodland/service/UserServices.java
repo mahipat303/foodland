d6fc40eeeb204ea77102fffdc3fa8a92308b7bc2
rahulrathore2110 <rahulg27727@gmail.com>
2022-12-16 15:08:23 +0530

    day3

--- foodland/src/main/java/com/foodland/service/UserServices.java
+++ foodland/src/main/java/com/foodland/service/UserServices.java
@@ -1,16 +1,24 @@
 package com.foodland.service;
 
+import com.foodland.exception.UserException;
 import org.springframework.stereotype.Service;
 
 import com.foodland.model.User;
 
 @Service
 public interface UserServices {
-	
-<<<<<<< HEAD
-	public User addUser(User user);
-=======
+
 	public User addUser(User user)throws UserException;
->>>>>>> fe5933cc0d08f84e3c2d115e06c279eac8893838
+
+	public User updateUser(User user)throws UserException;
+
+	public User removeUser(String mobile)throws UserException;
+
+	public User viewUser(String mobile)throws UserException;
+
+	public User viewAllUser()throws UserException;
+
+
+
 
 }
